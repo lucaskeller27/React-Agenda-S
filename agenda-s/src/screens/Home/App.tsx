a789092d053ef58114fc2aa9620e72751fef111a
Lucas <lucaskeller27@gmail.com>
2023-10-31 15:58:18 -0300

    First page done

--- agenda-s/src/screens/Home/App.tsx
+++ agenda-s/src/screens/Home/App.tsx
@@ -3,6 +3,11 @@
 // import viteLogo from '/vite.svg'
 import senaiLogo from '../../assets/sesi-senai.png'
 import logo from '../../assets/logo.png'
+import facebook from '../../assets/icons/facebook.svg'
+import youtube from '../../assets/icons/youtube.svg'
+import twitter from '../../assets/icons/twitter.svg'
+import linkedin from '../../assets/icons/linkedin.svg'
+import instagram from '../../assets/icons/instagram.svg'
 
 import './App.css'
 
@@ -22,36 +27,34 @@ function App() {
           </div>
       </header>
 
-      <p>Hello</p>
-
-      <div className="container">
-        <img id="logoSenai" src="./assets/sesi-senai.png" alt="SENAI"/>
-        <div className="buttonsDiv">
-            <p className="title">Faça seu Login</p>
-            <input className="input" type="text" placeholder="E-mail institucional"/>
-            <input className="input" type="password" placeholder="Senha"/>
-            <a href="./home.html"><button className="buttonBlue"><p className="buttonText">Entrar</p></button></a>
+        <div className="container">
+            <img src={senaiLogo} alt="SESI SENAI" className='senaiLogoMid'/>
+            <div className="buttonsDiv">
+                <p className="divTitle">Faça seu Login</p>
+                <input className="input" type="text" placeholder="E-mail institucional"/>
+                <input className="input" type="password" placeholder="Senha"/>
+                <a href="../Gerenciamento/App.tsx"><button className="buttonBlue"><p className="buttonText">Entrar</p></button></a>
+            </div>
         </div>
-      </div>
 
       <footer>
           <p className='footerText'>SENAI Florianópolis (CTAI)<br/>Fone: (48) 3239-5800<br/>
               SC-401, 3730 - Saco Grande, Florianópolis - SC, 88032-005</p>
-          <div id="logos">
+          <div className="logos">
               <a href="https://www.facebook.com/senaisc/" title="Ir para Facebook" target="_blank">
-                  <img className="logoImg grow" src="./assets/icons/facebook.svg" alt="Facebook"/>
+                    <img src={facebook} alt="Facebook" className='logoImg grow'/>
               </a>
               <a href="https://www.youtube.com/@senaiconhecimento" title="Ir para YouTube" target="_blank">
-                  <img className="logoImg grow" src="./assets/icons/youtube.svg" alt="YouTube"/>
+                    <img src={youtube} alt="YouTube" className='logoImg grow'/>
               </a>
               <a href="https://twitter.com/SENAIsc" title="Ir para Twitter" target="_blank">
-                  <img className="logoImg grow" src="./assets/icons/twitter.svg" alt="Twitter"/>
+                    <img src={twitter} alt="Twitter" className='logoImg grow'/>
               </a>
               <a href="https://www.linkedin.com/school/senai-sc/" title="Ir para LinkedIn" target="_blank">
-                  <img className="logoImg grow" src="./assets/icons/linkedin.svg" alt="LinkedIn"/>
+                    <img src={linkedin} alt="LinkedIn" className='logoImg grow'/>
               </a>
               <a href="https://www.instagram.com/senai.sc/" title="Ir para Instagram" target="_blank">
-                  <img className="logoImg grow" src="./assets/icons/instagram.svg" alt="Instagram"/>
+                    <img src={instagram} alt="Instagram" className='logoImg grow'/>
               </a>
           </div>
       </footer>
